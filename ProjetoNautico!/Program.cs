bd53113ccd872d35ac350f422cb601013bb630cf
hikmiau <166746547+hikmiau@users.noreply.github.com>
2026-07-01 19:13:59 -0300

    commit list

--- ProjetoNautico!/Program.cs
+++ ProjetoNautico!/Program.cs
@@ -10,6 +10,58 @@ namespace ProjetoNautico_
     {
         static void Main(string[] args)
         {
-        }
+
+            while (true)
+            {
+
+                Console.Clear();
+                Console.WriteLine("1. Novo Equipamento ");
+                Console.WriteLine("2. Listar Equipamentos");
+                Console.WriteLine("3. Remover Equipamentos");
+                Console.WriteLine("0. Sair");
+                int menu = int.Parse(Console.ReadLine());
+                if (menu == 0) break;
+
+                //switch (menu)
+                //{
+                //    case 1:
+                //        NovoCarro(lista_carros);
+                //        break;
+                //    case 2:
+                //        MostrarCarros(lista_carros);
+                //        break;
+                //    case 3:
+                //        RemoverCarro(lista_carros);
+                //        break;
+                //    default:
+                //        Console.WriteLine("Opção inválida.");
+                //        break;
+                //}
+                Console.ReadKey();
+            }
+
+            static void NovoEquipamento(List<String> lista_equipamentos){
+
+                int[] codigos = new int[10];
+                string[] nomes = new string[10];
+                string[] marcas = new string[10];
+                string[] modelos = new string[10];
+
+                int quantidade = 0;
+
+                Console.Write("Digite o Código do Equipamento: ");
+                codigos[quantidade] = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o Nome do Equipamento: ");
+                nomes[quantidade] = Console.ReadLine();
+
+                Console.Write("Digite a Marca do Equipamento: ");
+                marcas[quantidade] = Console.ReadLine();
+
+                Console.Write("Digite o Modelo do Equipamento: ");
+                modelos[quantidade] = Console.ReadLine();
+
+                quantidade++;
+            }
     }
 }
